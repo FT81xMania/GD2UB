b63b0a83fcfe22aa1eb14d7b33b507196183f6af
FT81x Manía <ft81xmania@gmail.com>
2017-09-20 09:31:17 -0500

    Update Set_up.ino

--- 2.SD_Externo/cobra/Set_up.ino
+++ 2.SD_Externo/cobra/Set_up.ino
@@ -3,7 +3,7 @@ void setup()
   GD.begin();
     //GD.cmd_setrotate(0);
    // GD.self_calibrate(); 
-  SD.begin(SD_CS, SD_SCK_MHZ(36));
+  SD.begin(SD_CS, SD_SCK_MHZ(FreqSPI2));
   
   CobraSTM32();
 }
